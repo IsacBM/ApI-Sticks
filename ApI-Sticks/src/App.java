92c8dfa556af9092c63f0f3fe1255da5d397f1e8
Isac B. Matos <91500212+IsacBM@users.noreply.github.com>
2022-07-19 16:25:29 -0300

    Update App.java

--- ApI-Sticks/src/App.java
+++ ApI-Sticks/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -22,16 +24,25 @@ public class App {
         List<Map<String, String>> listaDeDilmes = parser.parse(body);
 
 // Exibir e manipular os dados.
-        System.out.println("+-------------------------------------------------------");
-        for (Map<String, String> filme : listaDeDilmes) {
-            System.out.println("| " + filme.get("rank") + "- " + filme.get("title"));
+        System.out.println("+---------------------------------------------------------------");
+        var geradora = new GeradoraDeFigurinhas();
+        for (Map<String,String> filme : listaDeDilmes) {
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println("| Rank: " + filme.get("rank") + "  " + titulo);
+            //System.out.println();
             //imDbRatingCount crew System.out.println(filme.get("image"));
             System.out.println("| Nota: " + filme.get("imDbRating") + "                 " + "Ano de Lançamento: " + filme.get("year"));
             System.out.println("| Quantidade de Votos: " + filme.get("imDbRatingCount"));
             System.out.println("| Autores: " + filme.get("crew"));
-            System.out.println("|-----------------------[Poster]------------------------");
-            System.out.println("| " + filme.get("image"));
-            System.out.println("|-------------------------------------------------------");
+            System.out.println("|---------------------------------------------------------------");
         }
     }
 }
