04faef35798a5447ea967aac4173a54e2475ffa5
Isac B. Matos <91500212+IsacBM@users.noreply.github.com>
2022-07-22 14:24:22 -0300

    Add files via upload

--- ApI-Sticks/src/App.java
+++ ApI-Sticks/src/App.java
@@ -1,48 +1,48 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 public class App {
     public static void main(String[] args) throws Exception {
 
 // Realizar uma conexão HTTP: & Busacar os 'Top 250 Filmes'.
+        // API da IMDB;
         String url = "https://alura-filmes.herokuapp.com/conteudos";
-        URI endereco = URI.create(url);
-        var Client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = Client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-       // System.out.println(body);
+        // API da NASA;
+        //String url = "https://api.mocki.io/v2/549a5d8b/NASA-APOD";
+        
+        HttpCliente http = new HttpCliente();
+        String json = http.buscaDados(url);
 
 // Extrair só os dados interressantes (Titulo, Poster, classificação).
         var parser = new JsonParser();
-        List<Map<String, String>> listaDeDilmes = parser.parse(body);
+        List<Map<String, String>> listaDeConteudos = parser.parse(json);
 
 // Exibir e manipular os dados.
+        ExtratorConteudoApIDaNasa extrator = new ExtratorConteudoApIDaNasa();
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
         System.out.println("+---------------------------------------------------------------");
         var geradora = new GeradoraDeFigurinhas();
-        for (Map<String,String> filme : listaDeDilmes) {
+        //for (Map<String,String> conteudo : listaDeConteudos) {
+          for (int i = 0; i < 3; i++) {
+            Conteudo conteudo = conteudos.get(i);
 
-            String urlImagem = filme.get("image");
-            String titulo = filme.get("title");
-
-            InputStream inputStream = new URL(urlImagem).openStream();
-            String nomeArquivo = titulo + ".png";
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println("| Rank: " + filme.get("rank") + "  " + titulo);
+            System.out.println(conteudo.getTitulo());
+            System.out.println();
+
+          /*  System.out.println("| Rank: " + conteudo.get("rank") + "  " + titulo);
             //System.out.println();
             //imDbRatingCount crew System.out.println(filme.get("image"));
-            System.out.println("| Nota: " + filme.get("imDbRating") + "                 " + "Ano de Lançamento: " + filme.get("year"));
-            System.out.println("| Quantidade de Votos: " + filme.get("imDbRatingCount"));
-            System.out.println("| Autores: " + filme.get("crew"));
+            System.out.println("| Nota: " + conteudo.get("imDbRating") + "                 " + "Ano de Lançamento: " + conteudo.get("year"));
+            System.out.println("| Quantidade de Votos: " + conteudo.get("imDbRatingCount"));
+            System.out.println("| Autores: " + conteudo.get("crew"));
             System.out.println("|---------------------------------------------------------------");
+       */
         }
     }
 }
